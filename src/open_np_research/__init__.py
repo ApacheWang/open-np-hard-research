2c5ab00f201e961f7e8dfdacbd20b8891b0d9cfb
AWA <15575202581@163.com>
2026-07-23 17:38:26 -0700

    feat: add deterministic DPLL reference solver

--- src/open_np_research/__init__.py
+++ src/open_np_research/__init__.py
@@ -1,3 +1,13 @@
-from .sat3 import brute_force_solve, parse_dimacs_3sat, satisfies
+from .sat3 import (
+    brute_force_solve,
+    dpll_solve,
+    parse_dimacs_3sat,
+    satisfies,
+)
 
-__all__ = ["brute_force_solve", "parse_dimacs_3sat", "satisfies"]
+__all__ = [
+    "brute_force_solve",
+    "dpll_solve",
+    "parse_dimacs_3sat",
+    "satisfies",
+]
